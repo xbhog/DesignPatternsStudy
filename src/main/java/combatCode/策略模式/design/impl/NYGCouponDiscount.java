f0b1e695bceca364586fe6e12b4b37ba351a5952
xbhog <2251195014@qq.com>
2022-08-21 15:48:46 +0800

    增加策略模式实现

---
+++ src/main/java/combatCode/策略模式/design/impl/NYGCouponDiscount.java
@@ -0,0 +1,21 @@
+package combatCode.策略模式.design.impl;
+
+import combatCode.策略模式.design.ICouponDiscount;
+
+import java.math.BigDecimal;
+
+/**
+ * @author xbhog
+ * @describe:N元购
+ * @date 2022/8/21
+ */
+public class NYGCouponDiscount implements ICouponDiscount<Double> {
+    /**
+     * n元购购买
+     * 1. 无论原价多少钱都固定金额购买
+     */
+    @Override
+    public BigDecimal discountAmount(Double couponInfo, BigDecimal skuPrice) {
+        return new BigDecimal(couponInfo);
+    }
+}
--- src/test/java/openClosedPrincipleFile/TestDemo.java
+++ src/test/java/openClosedPrincipleFile/TestDemo.java
@@ -6,8 +6,17 @@ import SOLID5大原则.openClosedPrincipleFile.ICalculationArea;
 import SOLID5大原则.openClosedPrincipleFile.impl.CalculationAreaExt;
 import combatCode.模板模式.design.NetMall;
 import combatCode.模板模式.design.group.JDNetMall;
+import combatCode.策略模式.design.Context;
+import combatCode.策略模式.design.impl.MJCouponDiscount;
+import combatCode.策略模式.design.impl.NYGCouponDiscount;
+import combatCode.策略模式.design.impl.ZKCouponDiscount;
+import combatCode.策略模式.design.impl.ZjCouponDiscount;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author xbhog
@@ -22,10 +31,44 @@ public class TestDemo {
         double circular = calculationAreaExt.circular(2);
         System.out.println(circular);
     }
+
+    /**
+     * 测试模板模式
+     */
     @Test
     public void demo(){
         NetMall netMall = new JDNetMall("1000001","*******");
         String base64 = netMall.generateGoodsPoster("https://item.jd.com/100008348542.html");
         log.info("测试结果：{}", base64);
+    }
+    /**
+     * 测试策略模式
+     */
+    @Test
+    public void Strategy_mj(){
+        Context<Map<String, String>> mjActivity = new Context<>(new MJCouponDiscount());
+        HashMap<String, String> map = new HashMap<>();
+        map.put("x","100");
+        map.put("y","10");
+        BigDecimal discountAmount = mjActivity.discountAmount(map, new BigDecimal(100));
+        log.info("测试结果，满减优惠后的金额：{}",discountAmount);
+    }
+    @Test
+    public void Strategy_zj(){
+        Context<Double> context = new Context<>(new ZjCouponDiscount());
+        BigDecimal discountAmount = context.discountAmount(9D, new BigDecimal(100));
+        log.info("测试结果，直减优惠后的金额：{}",discountAmount);
+    }
+    @Test
+    public void Strategy_zk(){
+        Context<Double> context = new Context<>(new ZKCouponDiscount());
+        BigDecimal discountAmount = context.discountAmount(0.8D, new BigDecimal(100));
+        log.info("测试结果，折扣优惠后的金额：{}",discountAmount);
+    }
+    @Test
+    public void Strategy_nyg(){
+        Context<Double> context = new Context<>(new NYGCouponDiscount());
+        BigDecimal discountAmount = context.discountAmount(80D, new BigDecimal(100));
+        log.info("测试结果，N元购优惠后的金额：{}",discountAmount);
     }
 }
